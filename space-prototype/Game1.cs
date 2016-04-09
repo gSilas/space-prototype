e0073f66ec4ac57df37e4ff7e9f77a543f8cd210
Daniel Micheel <d.micheel@outlook.com>
2016-04-09 22:38:03 +0200

    basic Sound

--- space-prototype/Game1.cs
+++ space-prototype/Game1.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 
 namespace space_prototype
 {
@@ -11,6 +12,7 @@ namespace space_prototype
     public class Game1 : Game
     {
         GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
 
         //Camera/View information
         Vector3 cameraPosition = new Vector3(0.0f, -300.0f, 10.0f);
@@ -18,10 +20,13 @@ namespace space_prototype
         Matrix viewMatrix;
 
         SpriteFont motorwerk;
-        
+
         //Visual components
         Ship ship = new Ship();
         Asteroid asteroid = new Asteroid();
+
+        //Audio file
+        Song mainThemeSong;
 
         public Game1()
         {
@@ -37,9 +42,10 @@ namespace space_prototype
         /// </summary>
         protected override void Initialize()
         {
-            projectionMatrix = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45.0f),GraphicsDevice.DisplayMode.AspectRatio,1.0f, 1000.0f);
-            viewMatrix = Matrix.CreateLookAt(cameraPosition,Vector3.Zero, Vector3.Up);
-            asteroid.Position = new Vector3(0.1f,0f,0f);
+            projectionMatrix = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45.0f),
+                GraphicsDevice.DisplayMode.AspectRatio, 1.0f, 1000.0f);
+            viewMatrix = Matrix.CreateLookAt(cameraPosition, Vector3.Zero, Vector3.Up);
+            asteroid.Position = new Vector3(0.1f, 0f, 0f);
             base.Initialize();
         }
 
@@ -49,11 +55,15 @@ namespace space_prototype
         /// </summary>
         protected override void LoadContent()
         {
-          motorwerk = Content.Load<SpriteFont>("Fonts/motorwerk");
-          ship.Model= Content.Load<Model>("Models/torusknot");
-          ship.Transforms = SetupEffectDefaults(ship.Model);
-          asteroid.Model = Content.Load<Model>("Models/asteroid");
-          asteroid.Transforms = SetupEffectDefaults(asteroid.Model);
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            motorwerk = Content.Load<SpriteFont>("Fonts/motorwerk");
+            mainThemeSong = Content.Load<Song>("Audio/mainTheme");
+            ship.Model = Content.Load<Model>("Models/torusknot");
+            ship.Transforms = SetupEffectDefaults(ship.Model);
+            asteroid.Model = Content.Load<Model>("Models/asteroid");
+            asteroid.Transforms = SetupEffectDefaults(asteroid.Model);
+            MediaPlayer.Play(mainThemeSong);
         }
 
         /// <summary>
@@ -72,11 +82,12 @@ namespace space_prototype
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
+                Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            ship.Rotation += (float) gameTime.ElapsedGameTime.TotalMilliseconds* MathHelper.ToRadians(0.2f);
-            asteroid.Rotation += (float)gameTime.ElapsedGameTime.TotalMilliseconds * MathHelper.ToRadians(0.05f);
+            ship.Rotation += (float) gameTime.ElapsedGameTime.TotalMilliseconds*MathHelper.ToRadians(0.2f);
+            asteroid.Rotation += (float) gameTime.ElapsedGameTime.TotalMilliseconds*MathHelper.ToRadians(0.05f);
             base.Update(gameTime);
         }
 
@@ -87,14 +98,17 @@ namespace space_prototype
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            Matrix shipTransformMatrix = ship.RotationMatrix * Matrix.CreateTranslation(ship.Position);
-            Matrix asteroidTransformMatrix = asteroid.RotationMatrix * Matrix.CreateTranslation(asteroid.Position);
+            spriteBatch.Begin();
+            spriteBatch.DrawString(motorwerk, "Best Game!", Vector2.One, Color.Black);
+            spriteBatch.End();
+            Matrix shipTransformMatrix = ship.RotationMatrix*Matrix.CreateTranslation(ship.Position);
+            Matrix asteroidTransformMatrix = asteroid.RotationMatrix*Matrix.CreateTranslation(asteroid.Position);
             DrawModel(ship.Model, shipTransformMatrix, ship.Transforms);
             DrawModel(asteroid.Model, asteroidTransformMatrix, asteroid.Transforms);
 
             base.Draw(gameTime);
         }
+
         private Matrix[] SetupEffectDefaults(Model myModel)
         {
             Matrix[] absoluteTransforms = new Matrix[myModel.Bones.Count];
@@ -119,7 +133,7 @@ namespace space_prototype
             {
                 foreach (BasicEffect effect in mesh.Effects)
                 {
-                    effect.World = absoluteBoneTransform[mesh.ParentBone.Index] * modelTransform;
+                    effect.World = absoluteBoneTransform[mesh.ParentBone.Index]*modelTransform;
                 }
                 mesh.Draw();
             }
