5da34b643f4e7363a7a87750c95cd02c1985fdb3
Daniel Micheel <d.micheel@outlook.com>
2016-04-16 00:07:59 +0200

    started to work on collider

--- space-prototype/Entities/BoxCollider3D.cs
+++ space-prototype/Entities/BoxCollider3D.cs
@@ -1,7 +1,30 @@
-﻿namespace space_prototype.Entities
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace space_prototype.Entities
 {
     public class BoxCollider3D
     {
-        //TODO work out how this works
+        private BoundingBox _box;
+
+        public BoxCollider3D(Model model)
+        {
+            _box = GenerateBoundingBox(model);
+        }
+
+        private BoundingBox GenerateBoundingBox(Model toCalculate)
+        {
+            Vector3 min = Vector3.Zero;
+            Vector3 max = Vector3.Zero;
+            //TODO work out how this works
+            return new BoundingBox(min, max);
+        }
+
+        public bool Intersection(BoundingBox box)
+        {
+            return _box.Intersects(box); 
+        }
+
+        
     }
 }
